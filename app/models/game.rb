196ce840e5c1475aa6167ce3b59458c077942f90
Mark Mohr <markmohr15@gmail.com>
2014-06-23 18:58:28 -0400

    Import Schedule Locally

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,4 +11,25 @@ class Game < ActiveRecord::Base
     profiles.where(user_id: user.id).any?
   end
 
+  require 'csv'
+
+  def self.import()
+    CSV.foreach('cfbschedule.csv') do |row|
+      record = Game.new(
+        :visitor_id => row[0],
+        :home_id => row[1],
+        :visitor_rot => row[2],
+        :home_rot => row[3],
+        :neutral => row[4],
+        :visitor_score => row[5],
+        :home_score => row[6],
+        :closing_spread => row[7],
+        :date => row[8],
+        :created_at => row[9],
+        :updated_at => row[10]
+      )
+    record.save!
+    end
+  end
+
 end
