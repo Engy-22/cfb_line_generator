4d81d79ce3b0fe7b81b05631d0a711c1a6e456df
Mark Mohr <markmohr15@gmail.com>
2014-07-02 09:54:00 -0400

    Break schedule down by week

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,127 @@
 class GamesController < ApplicationController
 
-  def index
-    @games = Game.all.order('date').order('visitor_rot').page(params[:page])
-
-    #@results = @games.map do |game|
-     # thing = BusinessLogicThing.new(current_user, game.home, game.visitor)
-      #thing.calculate_result
-    #end
-
+  def wk1
+    @games = Game.where(:date => ("2014-08-27").."2014-09-01").order('date').order('visitor_rot')
     respond_to do |format|
       format.html
       format.json {render :json => @games}
     end
   end
 
+  def wk2
+    @games = Game.where(:date => ("2014-09-02").."2014-09-08").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk3
+    @games = Game.where(:date => ("2014-09-09").."2014-09-15").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk4
+    @games = Game.where(:date => ("2014-09-16").."2014-09-22").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk5
+    @games = Game.where(:date => ("2014-09-23").."2014-09-29").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk6
+    @games = Game.where(:date => ("2014-09-30").."2014-10-06").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk7
+    @games = Game.where(:date => ("2014-10-07").."2014-10-13").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk8
+    @games = Game.where(:date => ("2014-10-14").."2014-10-20").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk9
+    @games = Game.where(:date => ("2014-10-21").."2014-10-27").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk10
+    @games = Game.where(:date => ("2014-10-28").."2014-11-03").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk11
+    @games = Game.where(:date => ("2014-11-04").."2014-11-10").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk12
+    @games = Game.where(:date => ("2014-11-11").."2014-11-17").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk13
+    @games = Game.where(:date => ("2014-11-18").."2014-11-24").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk14
+    @games = Game.where(:date => ("2014-11-25").."2014-12-01").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
+
+  def wk15
+    @games = Game.where(:date => ("2014-12-02").."2014-12-14").order('date').order('visitor_rot')
+    respond_to do |format|
+      format.html
+      format.json {render :json => @games}
+    end
+  end
 end
+
+#@results = @games.map do |game|
+     # thing = BusinessLogicThing.new(current_user, game.home, game.visitor)
+      #thing.calculate_result
+    #end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,24 @@ Cfb::Application.routes.draw do
   resources :teams do
     resources :profiles
   end
-  resources :games
   resources :spreads, :only => [:create, :update]
   get "/pr", to: "profiles#pr"
   get "/hfa", to: "profiles#hfa"
   get "/about", to: "teams#about"
+  get "/wk1", to: "games#wk1"
+  get "/wk2", to: "games#wk2"
+  get "/wk3", to: "games#wk3"
+  get "/wk4", to: "games#wk4"
+  get "/wk5", to: "games#wk5"
+  get "/wk6", to: "games#wk6"
+  get "/wk7", to: "games#wk7"
+  get "/wk8", to: "games#wk8"
+  get "/wk9", to: "games#wk9"
+  get "/wk10", to: "games#wk10"
+  get "/wk11", to: "games#wk11"
+  get "/wk12", to: "games#wk12"
+  get "/wk13", to: "games#wk13"
+  get "/wk14", to: "games#wk14"
+  get "/wk15", to: "games#wk15"
   root to: "teams#index"
 end
