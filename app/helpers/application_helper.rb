b25dba8644528ed21527e4e15946bf6ec8c519e1
Mark Mohr <markmohr15@gmail.com>
2014-07-02 05:17:02 -0400

    Move PR Spread calculation to Helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  def get_spread
+     if game.neutral == true
+       game.visitor.profiles.find_by(user_id: current_user.id).power_ranking - game.home.profiles.find_by(user_id: current_user.id).power_ranking
+     else
+       game.visitor.profiles.find_by(user_id: current_user.id).power_ranking - game.home.profiles.find_by(user_id: current_user.id).power_ranking - game.home.profiles.find_by(user_id: current_user.id).hfa
+     end
+  end
+
 end
+
+
