9f40c42bcaa4b09ab50c1b100c827609205dd5cb
markmohr15 <mark@interfaceschool.com>
2018-08-02 23:37:32 -0500

    blank commit

--- app/models/game.rb
+++ app/models/game.rb
@@ -15,7 +15,7 @@
 #  created_at     :datetime
 #  updated_at     :datetime
 #
-
+#
 class Game < ActiveRecord::Base
   belongs_to :visitor, class_name: "Team"
   belongs_to :home, class_name: "Team"
